7a4c7ce2e12b32f5580234f433ecda984a9f7979
emilsjol <emil.sjolander11@gmail.com>
2024-06-03 11:55:11 +0200

    updated docs to not include non-working versions of code

--- menudata.js
+++ menudata.js
@@ -27,21 +27,6 @@ var menudata={children:[
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
-{text:"All",url:"globals.html",children:[
-{text:"c",url:"globals.html#index_c"},
-{text:"f",url:"globals.html#index_f"},
-{text:"i",url:"globals.html#index_i"},
-{text:"m",url:"globals.html#index_m"},
-{text:"n",url:"globals.html#index_n"},
-{text:"p",url:"globals.html#index_p"},
-{text:"r",url:"globals.html#index_r"},
-{text:"u",url:"globals.html#index_u"}]},
-{text:"Functions",url:"globals_func.html",children:[
-{text:"c",url:"globals_func.html#index_c"},
-{text:"f",url:"globals_func.html#index_f"},
-{text:"i",url:"globals_func.html#index_i"},
-{text:"m",url:"globals_func.html#index_m"},
-{text:"p",url:"globals_func.html#index_p"},
-{text:"r",url:"globals_func.html#index_r"},
-{text:"u",url:"globals_func.html#index_u"}]},
+{text:"All",url:"globals.html"},
+{text:"Functions",url:"globals_func.html"},
 {text:"Variables",url:"globals_vars.html"}]}]}]}
---
+++ search/all_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['print_5fmatrix_0',['print_matrix',['../finite__difference__convert_8cpp.html#ac452c5d1e893edbf8c93e54861d25b64',1,'print_matrix(vector&lt; vector&lt; double &gt; &gt; matrix):&#160;finite_difference_convert.cpp'],['../finite__difference__mpi__v2_8cpp.html#a5efc5719c651e35a55b0d335db24ad75',1,'print_matrix(vector&lt; double &gt; matrix, int cols):&#160;finite_difference_mpi_v2.cpp'],['../finite__difference__parallelized_8cpp.html#ac452c5d1e893edbf8c93e54861d25b64',1,'print_matrix(vector&lt; vector&lt; double &gt; &gt; matrix):&#160;finite_difference_parallelized.cpp']]],
+  ['print_5fvector_1',['print_vector',['../finite__difference__mpi__v2_8cpp.html#ab0986e7b7c3e0af149978d9b8bb6bf94',1,'finite_difference_mpi_v2.cpp']]]
+];
